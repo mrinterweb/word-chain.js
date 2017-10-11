fc82509485c9122eebcee7969e958bb07371147e
Sean McCleary <seanmcc@gmail.com>
2017-10-10 20:54:08 -0700

    Started writing a levenshtein alternative implementation, but pumping the breaks on that

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 'use strict';
 var fs = require('fs');
+// var levenshtein = require('levenshtein');
+
 /** Finds word chains
 * Example usage:
 */
+String.prototype.poorManLevenshtein(target) {
+  let source = this.toLowerCase().split('');
+  let target = target.toLowerCase().split('');
+
+  // todo - this probably won't work as order matters
+  return source.filter(function(char) { return target.indexOf(char) < 0; }).length;
+};
+
 class WordChain {
   constructor() {
     var contents = fs.readFileSync('./resources/wordlist.txt');
     this.wordList = contents.toString().split(/\n/);
   }
 
-  levenshteinDistance(source, target) {
-    if (source.length === 0) return target;
-    if (target.length === 0) return source;
 
-    let matrix = []
-
-  }
 }
 module.exports = WordChain;
